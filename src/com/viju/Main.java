29087a84f2f002c37dc29c13cac33f3adcde19ad
Viju Prakash <inboxtoviju@gmail.com>
2020-07-14 09:27:17 +0530

    This program was executed by M. Viju Prakash.

---
+++ src/com/viju/Main.java
@@ -0,0 +1,12 @@
+package com.viju;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // This java program demonstrates for each loop
+	String veg[] = {"Tomato", "Brinjal", "Onion", "Chilli", "Beans"};
+        for (String fruit: veg) {
+            System.out.println(fruit);
+        }
+    }
+}
